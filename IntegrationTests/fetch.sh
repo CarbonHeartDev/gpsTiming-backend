51aac98f585564fb158fbf524f2ac960e892cf94
Emiliano Sandri <emisandri@outlook.it>
2020-06-17 00:58:35 +0000

    Integration tests Added some commands to test front-end independently from back-end

---
+++ IntegrationTests/fetch.sh
@@ -0,0 +1,1 @@
+curl "http://localhost:666/route/$ROUTE_UUID" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0' -H 'Accept: */*' -H 'Accept-Language: it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'Referer: http://localhost:3000/' -H 'Origin: http://localhost:3000' -H 'DNT: 1' -H 'Connection: keep-alive'
